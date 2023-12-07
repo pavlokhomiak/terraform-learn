439ff4be56f969c93ab107f3de268a544a750d7b
Pavlo Khomiak <partner.Pavlo.Khomiak@philips.com>
2023-12-07 17:17:13 +0200

    use remote vpc module

--- main.tf
+++ main.tf
@@ -7,30 +7,29 @@ terraform {
   }
 }
 
-resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.vpc_cidr_block
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = var.vpc_cidr_block
+
+  azs                = [var.avail_zone]
+  public_subnets     = [var.subnet_cidr_block]
+  public_subnet_tags = { Name = "${var.env_prefix}-subnet-1" }
+
   tags = {
-    Name : "${var.env_prefix}-vpc"
+    Name = "${var.env_prefix}-vpc"
   }
-}
-
-module "myapp-subnet" {
-  source = "./modules/subnet"
-
-  subnet_cidr_block = var.subnet_cidr_block
-  avail_zone        = var.avail_zone
-  env_prefix        = var.env_prefix
-  vpc_id            = aws_vpc.myapp-vpc.id
 }
 
 module "myapp-server" {
   source = "./modules/webserver"
 
-  vpc_id              = aws_vpc.myapp-vpc.id
+  vpc_id              = module.vpc.vpc_id
   my_ip               = var.my_ip
   env_prefix          = var.env_prefix
   public_key_location = var.public_key_location
   instance_type       = var.instance_type
-  subnet_id           = module.myapp-subnet.my-subnet.id
+  subnet_id           = module.vpc.public_subnets[0]
   avail_zone          = var.avail_zone
 }
